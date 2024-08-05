6401c7b72e977f877acc364cd9baa80c5c2b4b52
Akila Pilapitiya <lakshanakila3@gmail.com>
2024-08-05 20:06:36 +0530

    Code changes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
-#include<iostream>
+
+// TASK MAP
+// Design - 
+// menus
+// Input handling  - chanuka
+
+
+
+// TO LEARN
+// Keyboard realtime inputs
+
+
+
+
+
+
+
+
+
+// L I B R A R I E S
+#include <iostream>
 using namespace std;
 
+
+// F U N C T I O N S
 int main(){
     cout << "Hello word";
 }
+
