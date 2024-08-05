5c30eca16b62fff5f29b0ffe2341e59b137fcac5
Akila Pilapitiya <lakshanakila3@gmail.com>
2024-08-05 22:17:54 +0530

    code commits

--- main.cpp
+++ main.cpp
@@ -1,29 +1,82 @@
 
-// TASK MAP
-// Design - 
-// menus
-// Input handling  - chanuka
-
-
-
-// TO LEARN
-// Keyboard realtime inputs
-
-
-
-
-
-
-
-
-
-// L I B R A R I E S
+//                      L I B R A R I E S
 #include <iostream>
+#include <chrono> // Time the tasks
+#include <cstdlib> // For system configs
 using namespace std;
 
+//                  G L O B A L  V A R I A B L E S
 
-// F U N C T I O N S
-int main(){
-    cout << "Hello word";
+
+
+
+// Function List
+void welcomeSplashScreen();      // Splash Screen at Initialization                     == AKILA
+void screenClear();              // Switch and clear terminal based on time intervals   == AKILA
+
+
+
+//                      F U N C T I O N S
+
+void screenClear(int time){
+    auto duration = std::chrono::seconds(time);                 //Set Time in which changes occur
+    auto start_time = std::chrono::steady_clock::now();         // Obtain Current time
+    while ((std::chrono::steady_clock::now() - start_time) < duration) { 
+        //Loop to idle while in Sllep time
+    }
+    system("cls");
 }
 
+void welcomeSplashScreen(){
+    system("cls");
+    //cout << "************************************************************************************************************************" << endl;
+    cout << "       ************     ***              ************     ************     ************      ***      ***               " << endl;
+    cout << "       ************     ***              ************     ************     ************      ***      ***               " << endl;
+    cout << "       ***              ***              ***      ***     ***      ***     ***      ***      ***      ***               " << endl;
+    cout << "       ************     ***              ************     ************     ************      ************               " << endl;
+    cout << "       ************     ***              ************     ************     ************      ************               " << endl;
+    cout << "       ***              ***              ***      ***     ***              ***                        ***               " << endl;
+    cout << "       ***              ************     ***      ***     ***              ***               ************               " << endl;
+    cout << "       ***              ************     ***      ***     ***              ***               ************               " << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << "       ************     ************     ************     ************                                                  " << endl;
+    cout << "       ************     ************     ************     ************                                                  " << endl;
+    cout << "       ***      ***         ***          ***      ***     ***      ***                                                  " << endl;
+    cout << "       ************         ***          ***      ***     ***      ***                                                  " << endl;
+    cout << "       ************         ***          ************     ***      ***                                                  " << endl;
+    cout << "       ***      ***         ***          *********        ***      ***                                                  " << endl;
+    cout << "       ************     ************     ***     ***      ************                                                  " << endl;
+    cout << "       ************     ************     ***      ****    ************                                                  " << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << "                             ******************" << endl;
+    cout << "                          ******************"                     << endl;
+    cout << "          ******         ****************"  << endl;
+    cout << "       *****  ****     **************"  << endl;
+    cout << "         *********  *************         **********"  << endl;
+    cout << "          ***************************************"  << endl;
+    cout << "             **********************************"  << endl;
+    cout << "               *********************************"  << endl;
+    cout << "                  *******************************"  << endl;
+    cout << "                        *****************"  << endl;
+    cout <<"--------------------------------------------------------------------------------------------------------------------------"<< endl;
+
+
+
+
+
+
+
+    //screenClear(3);
+    //cout << "Time out";
+}
+
+
+int main(){
+    welcomeSplashScreen();
+    return(0);
+
+}
