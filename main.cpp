5fe0c08400f6ad1503da8ac107ad5f2322c690c1
Akila Pilapitiya <lakshanakila3@gmail.com>
2024-08-06 19:46:55 +0530

    code changes

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <thread>          //For Code Sleep
 #include <chrono>          //To make the code sleep
 #include <cstdlib>         // For system()
+#include <fstream>         //File Handling
 using namespace std;
 
 
@@ -20,9 +21,14 @@ int mainMenuUserInput = 0;  //Store the users Input from thew Main Menu,
 
 
                                     // G L O B A L I Z E   A L L  F U N C T I O N S
+
 void gameFrame();          //Draw the Game Frame
-void mainMenu();           //Main Menu Configurations
+int mainMenu();            //Main Menu Configurations
+void returnToMainMenu();   //Function to Return to the Main Menu from Menu Elements
 void splashScreen();       //Splash Screen Design for the Game
+void creditsPage();        //Credits Page from main Menu
+void scoreFileReader();    //Reads the file used to store Scores
+void gameInstructions();   //Instruction Page for the game
 
 
 
@@ -38,25 +44,37 @@ void gameFrame(){//AKILA
     for(int k = 0; k < frameWidth; k++) cout <<"-";
 }
 
-void mainMenu(){//AKILA
+int mainMenu(){//AKILA
+    system("cls");
     cout << "Game Menu";
     cout << "1 - New Game, 2 - Scores, 3 - Instructions, 4 - Credits,  5 - Exit ";
     cin >> mainMenuUserInput;
     switch(mainMenuUserInput){
         case (1): cout << "Hello";  //New Game Connection
-        case (2): cout << "Hello"; //Score List Connection
-        case (3): cout << "Hello"; //Instruction Connection
-        case (4): cout << "Hello"; //Credits Connection
-        case (5): cout << "Hello"; //Exit Connection
+        case (2): scoreFileReader(); //Score List Connection
+        case (3): gameInstructions(); //Instruction Connection
+        case (4): creditsPage(); //Credits Connection
+        case (5): break; //Exit Connection
         default:
             cout << "Enter a valid Input" << endl; 
-            cin >> mainMenuUserInput;
-
-        
+            cin >> mainMenuUserInput;       
     }
 }
 
-void splashScreen(){
+void returnToMainMenu(){//AKILA
+    cout << "\n";
+    cout << "Enter 0 to return to main Menu" ;
+    cin  >> mainMenuUserInput;
+    while(mainMenuUserInput != 0){
+        cout << "Enter a valid Input" ;
+        cin  >> mainMenuUserInput;
+    }
+    if(mainMenuUserInput == 0){
+        mainMenu(); //link to mainMenu Function
+    }
+}
+
+void splashScreen(){//AKILA
     for(int i = 0; i < frameWidth; i++) cout <<"-";
     cout << endl;
     
@@ -72,8 +90,8 @@ void splashScreen(){
     cout << "|" << "       BALLBOUNCEBALLBOp    LBB     UNl      BALb     EBAb                                        " << "|" << endl;
     cout << "|" << "       BALLBOUNCEBALLBp     LBBALLBOUNCEb    BALBl    EBALl                                       " << "|" << endl;
     cout << "|" << "                                                                                                  " << "|" << endl;
-    cout << "|" << "                                                                                                  " << "|" << endl;
-    cout << "|" << "      BALLBOUNCEBALb                                                                              " << "|" << endl;
+    cout << "|" << "                                                                                                 " << "|" << endl;
+    cout << "|" << "       BALLBOUNCEBALb                                                                             " << "|" << endl;
     cout << "|" << "       BALLBOUNCEBALLBb                                                                           " << "|" << endl;
     cout << "|" << "       BALL         LBOb                                                                          " << "|" << endl;
     cout << "|" << "       BALL        LLBOp                             LLBO0                                        " << "|" << endl;
@@ -81,7 +99,7 @@ void splashScreen(){
     cout << "|" << "       BALLBOUNCEBALLb    lLLB    BOUN  BALl    LLBl   lCEBALLBBALl   UNCE   LBBA   OUN     LBBl  " << "|" << endl;
     cout << "|" << "       BALL        LLBOb  lLLB    BOUN  BALl    LLBl   lCEB    BALl   UNC           OUN     LBB0  " << "|" << endl;
     cout << "|" << "       BALL         LBOU  lLLB    BOUN  BALl    LLBl   lCEB    BALl   UNC           OUNCEBALLB0   " << "|" << endl;
-    cout << "|" << "       BALLBOUNCEBALLBOp  lLLB   LBOUN  BALL0  ALLBl   lCEB    BALl   UNCE    BBA   OUN0          " << "|" << endl;
+    cout << "|" << "       BALLBOUNCEBALLBOp  lLLB   LBOUN  BALL0  ALLBl   lCEB    BALl   UNCE   LBBA   OUN0          " << "|" << endl;
     cout << "|" << "       BALLBOUNCEBALLBp    0LBBALLBOU    0ALLBBALL0    lCEB    BALl    NCEBALLBBA    UNCEBALLBB   " << "|" << endl;
     cout << "|" << "                                                                                                  " << "|" << endl;
     cout << "|" << "                                                                                                  " << "|" << endl;
@@ -90,9 +108,28 @@ void splashScreen(){
 
 
     this_thread::sleep_for(chrono::seconds(splashScreenRefresh));  //sleep console for specified time
-    system("cls");
     mainMenu(); //link to mainMenu Function
 
+}
+
+
+void creditsPage(){//AKILA
+    system("cls");
+    cout << "Team Details" << endl;
+    returnToMainMenu(); 
+}
+
+
+void scoreFileReader(){//AKILA
+    system("cls");
+    cout << "Display Scorecard";
+    returnToMainMenu();
+}
+
+void gameInstructions(){
+    system("cls");
+    cout << "Display Game Instructions";
+    returnToMainMenu();
 }
 
 
